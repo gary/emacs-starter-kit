9b7013de152c813074342e5808c84646569d949f
Gary Iams <ge.iams@gmail.com>
2012-10-10 14:56:14 -0400

    Setup dedicated file for customizations made through customize.

--- gary.el
+++ gary.el
@@ -1,5 +1,7 @@
 ;;; gary.el
 
+(setq custom-file (concat esk-user-dir "/custom.el"))
+
 ;; FIXME: void variable ido-kill-emacs-hook wtf?
 (add-hook 'coding-hook 'turn-on-visual-line-mode)
 
---
+++ gary/custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(exec-path (quote ("/usr/local/bin" "/usr/bin" "/bin" "/usr/sbin" "/sbin" "/usr/local/Cellar/emacs/HEAD/libexec/emacs/24.1.50/i386-apple-darwin11.4.0"))))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
