2cf8fd24b07ed0da289e24224cdd1e18beaac69e
Gary Iams <ge.iams@gmail.com>
2012-10-12 18:55:23 -0400

    Grandfather register access from Starter Kit version 1.

--- gary/registers.el
+++ gary/registers.el
@@ -1,1 +1,14 @@
-(dolist (r `((?d (file . ,(concat esk-user-dir "/starter-kit-defuns.el"))))))
+;;; gary/registers.el --- Set up registers
+
+;; Registers allow you to jump to a file or other location
+;; quickly. Use C-x r j followed by the letter of the register (i for
+;; init.el, r for this file) to jump to it.
+
+(dolist (r `((?i (file . ,(concat user-emacs-directory "init.el")))
+             (?b (file . ,(concat esk-user-dir "/bindings.el")))
+             (?m (file . ,(concat esk-user-dir "/misc.el")))
+             (?r (file . ,(concat esk-user-dir "/registers.el")))))
+  (set-register (car r) (cadr r)))
+
+(provide 'gary/registers)
+;;; gary/registers.el ends here
