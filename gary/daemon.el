445d65750a1259d2d68e015a51c58bebb881d1be
Gary Iams <ge.iams@gmail.com>
2012-10-13 20:15:38 -0400

    Unbind default bindings for exiting and suspending when daemonized.

--- gary/daemon.el
+++ gary/daemon.el
@@ -2,5 +2,11 @@
 
 (add-hook 'after-make-frame-functions 'init-ui)
 
+(if (daemonp)
+    (progn
+      (global-unset-key (kbd "C-x C-c"))
+      (global-unset-key (kbd "C-z"))
+      (global-unset-key (kbd "C-x C-z"))))
+
 (provide 'gary/daemon)
 ;;; gary/daemon.el ends here
