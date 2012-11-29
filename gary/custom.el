544cc863448ccf1deeb2cc2fa932435d3c378c22
Gary Iams <ge.iams@gmail.com>
2012-11-29 17:48:40 -0500

    Move recentf's save file to variable data dir.

--- gary/custom.el
+++ gary/custom.el
@@ -5,6 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(exec-path (quote ("/Users/gary/.rbenv/shims/" "/Users/gary/.rbenv/bin" "/Users/gary/bin" "/usr/local/bin" "/usr/bin" "/bin" "/usr/local/sbin" "/usr/sbin" "/sbin" "/usr/X11/bin" "/usr/local/Cellar/emacs/HEAD/libexec/emacs/24.1.50/i386-apple-darwin11.4.0")))
  '(feature-cucumber-command "bin/rake cucumber CUCUMBER_OPTS=\"{options}\" FEATURE=\"{feature}\"")
+ '(recentf-save-file "~/.emacs.d/var/recentf")
  '(save-place-file "~/.emacs.d/var/places")
  '(server-auth-dir "~/.emacs.d/var/server/")
  '(tramp-persistency-file-name "/Users/gary/.emacs.d/var/tramp"))
