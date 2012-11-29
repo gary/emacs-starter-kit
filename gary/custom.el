3e7da98e159a459bbbf40ff916acf697181728ed
Gary Iams <ge.iams@gmail.com>
2012-11-29 18:04:34 -0500

    Move Ispell's personal dictionary to var.

--- gary/custom.el
+++ gary/custom.el
@@ -7,6 +7,7 @@
  '(exec-path (quote ("/Users/gary/.rbenv/shims/" "/Users/gary/.rbenv/bin" "/Users/gary/bin" "/usr/local/bin" "/usr/bin" "/bin" "/usr/local/sbin" "/usr/sbin" "/sbin" "/usr/X11/bin" "/usr/local/Cellar/emacs/HEAD/libexec/emacs/24.1.50/i386-apple-darwin11.4.0")))
  '(feature-cucumber-command "bin/rake cucumber CUCUMBER_OPTS=\"{options}\" FEATURE=\"{feature}\"")
  '(ido-save-directory-list-file "~/.emacs.d/var/ido.last")
+ '(ispell-personal-dictionary "~/.emacs.d/var/ispell_english")
  '(recentf-save-file "~/.emacs.d/var/recentf")
  '(save-place-file "~/.emacs.d/var/places")
  '(server-auth-dir "~/.emacs.d/var/server/")
