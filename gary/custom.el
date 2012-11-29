aa546f5a4b6143ffe693990fc7b7ec6ba628699b
Gary Iams <ge.iams@gmail.com>
2012-11-29 17:49:53 -0500

    Move bookmark save file to variable data dir.

--- gary/custom.el
+++ gary/custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(bookmark-default-file "/Users/gary/.emacs.d/var/emacs.bmk")
  '(exec-path (quote ("/Users/gary/.rbenv/shims/" "/Users/gary/.rbenv/bin" "/Users/gary/bin" "/usr/local/bin" "/usr/bin" "/bin" "/usr/local/sbin" "/usr/sbin" "/sbin" "/usr/X11/bin" "/usr/local/Cellar/emacs/HEAD/libexec/emacs/24.1.50/i386-apple-darwin11.4.0")))
  '(feature-cucumber-command "bin/rake cucumber CUCUMBER_OPTS=\"{options}\" FEATURE=\"{feature}\"")
  '(recentf-save-file "~/.emacs.d/var/recentf")
