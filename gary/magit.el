3a175bb9d5329eeb7fc97d174d40d8cf0645fc26
Gary Iams <ge.iams@gmail.com>
2014-05-31 18:18:27 -0400

    Reformat Magit customizatons from fc34073

--- gary/magit.el
+++ gary/magit.el
@@ -1,7 +1,7 @@
 ;;; gary/magit.el
 
-(customize-save-variable 'magit-default-tracking-name-function (quote magit-default-tracking-name-branch-only))
-(customize-save-variable 'magit-use-overlays nil)
+(setq magit-default-tracking-name-function (quote magit-default-tracking-name-branch-only)
+      magit-use-overlays nil)
 
 (provide 'gary/magit)
 ;;; gary/magit.el ends here
