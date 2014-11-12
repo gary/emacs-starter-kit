e803926a5ab188309f8ddb1eb9b3d5b48e5032b1
Gary Iams <ge.iams@gmail.com>
2014-11-12 10:14:23 -0500

    Add rspec integration

--- gary/ruby.el
+++ gary/ruby.el
@@ -7,10 +7,22 @@
   (progn
     (global-rbenv-mode)
     (setq enh-ruby-program rbenv-ruby-shim)))
+(eval-after-load 'rspec-mode
+  '(rspec-install-snippets))
 (eval-after-load 'ruby-end (ruby-end-mode))
+
+(defadvice rspec-compile (around rspec-compile-around)
+  "Use BASH shell for running the specs because of ZSH issues."
+  (let ((shell-file-name "/bin/bash"))
+    ad-do-it))
+
+(ad-activate 'rspec-compile)
 
 (add-hook 'enh-ruby-mode-hook 'ri-bind-key)
 (add-hook 'enh-ruby-mode-hook 'rbenv-use-corresponding)
+(add-hook 'enh-ruby-mode-hook 'rspec-mode)
+
+(customize-set-variable 'rspec-use-rbenv t)
 
 (setq enh-ruby-program rbenv-ruby-shim)
 
