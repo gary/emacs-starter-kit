1f623c4eec4fa4fa7d0fc1c039cf07e93e212143
Gary Iams <ge.iams@gmail.com>
2014-11-12 10:13:45 -0500

    Fix insertion of "end" when editing ruby

--- gary/ruby.el
+++ gary/ruby.el
@@ -3,17 +3,11 @@
 (defun ri-bind-key ()
   (local-set-key [f1] 'yari-anything))
 
-;;; ruby-mode 1.0 relic
-(defun ruby-insert-end ()
-  (interactive)
-  (insert "end")
-  (ruby-indent-line t)
-  (end-of-line))
-
 (eval-after-load 'rbenv
   (progn
     (global-rbenv-mode)
     (setq enh-ruby-program rbenv-ruby-shim)))
+(eval-after-load 'ruby-end (ruby-end-mode))
 
 (add-hook 'enh-ruby-mode-hook 'ri-bind-key)
 (add-hook 'enh-ruby-mode-hook 'rbenv-use-corresponding)
